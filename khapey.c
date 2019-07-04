3551220aaccdc26423e455172f751fa779d8d65a
bibe29 <40533186+bibe29@users.noreply.github.com>
2019-07-04 08:00:48 +0545

    Add files via upload

---
+++ khapey.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main(){
+	char pn[10][10],t[10];
+	int arr[10],bur[10],star[10],finish[10],tat[10],wt[10],i,j,n,temp;
+	int totwt=0,totat=0;
+	printf("enter no of process:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		printf("enter process name,arrival time & burst time:");
+		scanf("%s%d%d",&pn[i],&arr[i],&bur[i]);
+	}
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(arr[i]<arr[j]){
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+				temp=bur[i];
+				bur[i]=bur[j];
+				bur[j]=temp;
+				strcpy(t,pn[i]);
+				strcpy(pn[i],pn[j]);
+				strcpy(pn[j],t);
+			}
+		}
+	}
+	for(i=0;i<n;i++){
+		if(i==0){
+			star[i]=arr[i];
+		}
+		else{
+			star[i]=finish[i-1];
+		}
+		wt[i]=star[i]-arr[i];
+		finish[i]=star[i]+bur[i];
+		tat[i]=finish[i]-arr[i];
+	}
+	printf("\n name arrTime burTime WaitTime start TAT Finish");
+	for(i=0;i<n;i++){
+		printf("\n%s\t%3d\t%3d\t%3d\t%3d\t%6d\t%6d",pn[i],arr[i],bur[i],wt[i],
+		star[i],tat[i],finish[i]);
+		totwt +=wt[i];
+		totat +=tat[i];
+	}
+	printf("\n average waiting time:%f",(float)totwt/n);
+	printf("\n average turn around time:%f",(float)totat/n);
+	getch();
+}
